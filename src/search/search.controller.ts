e2d57414df4a07a5554f4e97fa4eca0691a6226b
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2020-12-25 05:36:33 +0100

    chore (*): clean up
    - Fixed issues with search controller implementation

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpStatus, Post, Query } from '@nestjs/common';
 import { OpportunitySearchApiResponse } from 'src/interface/opportunity-search-api.interface';
 import { PersonSearchApiResponse } from 'src/interface/person-search-api.interface';
 import { SearchService } from './search.service';
@@ -13,13 +13,17 @@ export class SearchController {
   async getPeopleResults(
     @Body('name') name: string,
     @Body('skills') skills: [] = [],
-    @Body('experience') experience: string,
     @Query('offset') offset: string = "0"
   ): Promise<PersonSearchApiResponse>{
+    if (!name && skills.length < 1) {
+      throw new BadRequestException({
+        error: 'Either a name of skill has to be passed',
+        status: HttpStatus.BAD_REQUEST
+      })
+    }
     return await this.searchService?.getPersonSearchResults(offset, {
       name,
-      skills,
-      experience
+      skills
     })
   }
 
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -14,40 +14,48 @@ export class SearchService {
 
   async getPersonSearchResults(offset: string = '0', body: {
     name?: string,
-    skills?: [],
-    experience?: string
+    skills?: string[]
   }): Promise<PersonSearchApiResponse>{
     try {
       const url = new URL(`${this.http.axiosRef.defaults.baseURL}/people/_search/`)
       url.searchParams.append('offset', offset)
       let requestData = {}
-      if (!body?.experience && body.skills.length < 1) {
+      this.logger.verbose(body.skills)
+      if (body?.name && body.skills.length < 1) {
         requestData = {
           "name": {
             "term": body?.name
           }
         }
+      } else if(!body?.name && body.skills.length > 0 && body.skills.length < 2) {
+        requestData = {
+          "skill/role": {
+            "text": body.skills[0],
+            "experience": "1-plus-year"
+          }
+        }
       } else {
         requestData = {
           "and": [
-            {
+            body?.name ? {
               "name": {
                 "term": body?.name
               }
-            },
+            } : false,
             ...body.skills.map(skill => ({
               "skill/role": {
                 "text": skill,
-                "experience": `${body.experience ?? 1}-plus-year`
+                "experience": `1-plus-year`
               }
             }))
-          ]
+          ].filter(Boolean)
         }
       }
-      Logger.verbose(requestData)
+      this.logger.verbose(requestData)
       const data: AxiosResponse<PersonSearchApiResponse> = await this.http.post<PersonSearchApiResponse>(url.href, requestData).toPromise()
       return data?.data
     } catch(error) {
+      this.logger.verbose(error)
       throw new NotFoundException({
         status: (error as AxiosError)?.response?.status,
         error: (error as AxiosError)?.response?.data?.message || 'Invalid search query'
